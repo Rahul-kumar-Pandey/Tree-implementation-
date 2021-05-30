2d0ff7c56d9a356e16a3aec4ec2db4aea6287b75
Rahul Kumar Pandey <77538550+Rahul-kumar-Pandey@users.noreply.github.com>
2021-05-30 17:06:28 +0530

    Create withoutClassImplementation.cpp

---
+++ withoutClassImplementation.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct node{
+  int data;
+  node *left;
+  node *right;
+};
+
+/* Function to create a new node */
+ 
+Node* CreateNode(int data)
+{
+    Node* newNode = new Node();
+    if (!newNode) {
+        cout << "Memory error\n";
+        return NULL;
+    }
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+ 
+Node* InsertNode(Node* root, int data)
+{
+    // If the tree is empty, assign new node address to root
+    if (root == NULL) {
+        root = CreateNode(data);
+        return root;
+    }
+ 
+    // Else, do level order traversal until we find an empty
+    // place, i.e. either left child or right child of some
+    // node is pointing to NULL.
+    queue<Node*> q;
+    q.push(root);
+ 
+    while (!q.empty()) {
+        Node* temp = q.front();
+        q.pop();
+ 
+        if (temp->left != NULL)
+            q.push(temp->left);
+        else {
+            temp->left = CreateNode(data);
+            return root;
+        }
+ 
+        if (temp->right != NULL)
+            q.push(temp->right);
+        else {
+            temp->right = CreateNode(data);
+            return root;
+        }
+    }
+}
+ 
+/* Inorder traversal 
+void recursiveInorder(Node *root){
+  
+}
+int main(){
+  node *root=CreateNode(10);
+  return 0;
+}
